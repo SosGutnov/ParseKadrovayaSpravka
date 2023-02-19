ba5256bea249bcaf4afa7d18ee23b3e174ad7e48
SosGutnov <soslan.gutnov157@gmail.com>
2023-02-19 15:54:41 +0300

    Добавьте файлы проекта.

---
+++ ConnectFile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParseKadrovayaSpravka
+{
+    class ConnectFile
+    {
+        public static string XlPath { get; set; }
+
+        public static void Connect()
+        {
+            var curDir = Environment.CurrentDirectory;
+            XlPath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = curDir + @"\..\..";
+                //openFileDialog.Filter = "(*.xlsm)|*.xlsm|(*.xlsx)|*.xlsx";
+                openFileDialog.Filter = "(*.xls)|*.xls*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+                openFileDialog.Title = "Выберите файл";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    XlPath = openFileDialog.FileName;
+                }
+            }
+
+            if (XlPath == string.Empty)
+            {
+                MessageBox.Show("плохой файл");
+                return;
+            }
+
+        }
+    }
+}
---
+++ ParseTeacherInfo.cs
@@ -0,0 +1,30 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParseKadrovayaSpravka
+{
+    class ParseTeacherInfo
+    {
+        public static void ParseTacherInfo()
+        {
+            var book = new XLWorkbook(ConnectFile.XlPath);
+
+            var lists = book.Worksheets;
+
+            foreach (var list in lists)
+            {
+                if (list.Name == "Сведения о преподавателях")
+                {
+                    var a = list.Cell(1,1).Value;
+                    MessageBox.Show(a.ToString());
+                }
+            }
+
+        }
+    }
+}
