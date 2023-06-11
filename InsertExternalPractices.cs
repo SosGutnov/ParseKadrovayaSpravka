40b95aa9e1acba19e5bbf8d77b74e1ce6e7f0f60
SosGutnov <soslan.gutnov157@gmail.com>
2023-06-11 20:11:35 +0300

    extpractices2

--- InsertExternalPractices.cs
+++ InsertExternalPractices.cs
@@ -58,45 +58,28 @@ namespace ParseKadrovayaSpravka
                         }
                     }
 
-                    string sql = "SELECT `id` FROM `external_practices` WHERE `empl_id`= @empl_id" +
-                            " AND `date_from`= @date_from" +
-                            " AND `date_to`= @date_to" +
-                            " AND `organization`= @organization" +
-                            " AND `position`= @position" +
-                            " AND `education`= @education";
-                    MySqlCommand cmd1 = new MySqlCommand(sql, Form1.connection);
-                    cmd1.Parameters.Add("@empl_id", MySqlDbType.Int32).Value = id_employee;
-                    cmd1.Parameters.Add("@date_from", MySqlDbType.DateTime).Value = date_from;
-                    cmd1.Parameters.Add("@date_to", MySqlDbType.DateTime).Value = date_to;
-                    cmd1.Parameters.Add("@organization", MySqlDbType.VarChar).Value = organization;
-                    cmd1.Parameters.Add("@position", MySqlDbType.VarChar).Value = position;
-                    cmd1.Parameters.Add("@education", MySqlDbType.Int32).Value = education;
-
-                    MySqlDataReader reader1 = cmd1.ExecuteReader();
-                    if (reader1.HasRows)
+                    string[] organizations = organization.Split(new string[1] { "\n" }, StringSplitOptions.None);
+                    int z = 0;
+                    for (int j = 0; j < organizations.Length; j++)
                     {
-                        while (reader1.Read())
+                        string org = organizations[j];
+                        string[] positions = position.Replace("\n\n", " ").Split();
+
+                        if (positions.Length >= z + 1)
                         {
+                            string pos = positions[z];
+                            //MySqlFunctions.SetExtPractices(id_employee, date_from, date_to, org, pos, education);
+                            z++;
                         }
-                        reader1.Close();
-                    }
-                    else
-                    {
-                        reader1.Close();
-                        sql = "INSERT INTO `external_practices` (`empl_id`, `date_from`, `date_to`, `organization`, `position`, `education`)"
-                                                 + " VALUES (@empl_id, @date_from, @date_to, @organization, @position, @education) ";
-                        MySqlCommand cmd2 = new MySqlCommand(sql, Form1.connection);
-
-                        cmd2.Parameters.Add("@empl_id", MySqlDbType.Int32).Value = id_employee;
-                        cmd2.Parameters.Add("@date_from", MySqlDbType.DateTime).Value = date_from;
-                        cmd2.Parameters.Add("@date_to", MySqlDbType.DateTime).Value = date_to;
-                        cmd2.Parameters.Add("@organization", MySqlDbType.VarChar).Value = organization;
-                        cmd2.Parameters.Add("@position", MySqlDbType.VarChar).Value = position;
-                        cmd2.Parameters.Add("@education", MySqlDbType.Int32).Value = education;
-
-                        cmd2.ExecuteNonQuery();
+                        if (positions.Length >= z + 1)
+                        {
+                            string pos = positions[z];
+                            //MySqlFunctions.SetExtPractices(id_employee, date_from, date_to, org, pos, education);
+                            z++;
+                        }
                         
                     }
+                    
                 }
                 Console.WriteLine("external_practice - good");
             }
@@ -110,4 +93,6 @@ namespace ParseKadrovayaSpravka
 
         }
     }
+
+
 }
--- MySqlFunctions.cs
+++ MySqlFunctions.cs
@@ -29,5 +29,48 @@ namespace ParseKadrovayaSpravka
             reader.Close();
             return id;
         }
+
+        public static void SetExtPractices(int id_employee, DateTime date_from, DateTime date_to, string organization, string position, int education)
+        {
+            string sql = "SELECT `id` FROM `external_practices` WHERE `empl_id`= @empl_id" +
+                            " AND `date_from`= @date_from" +
+                            " AND `date_to`= @date_to" +
+                            " AND `organization`= @organization" +
+                            " AND `position`= @position" +
+                            " AND `education`= @education";
+            MySqlCommand cmd1 = new MySqlCommand(sql, Form1.connection);
+            cmd1.Parameters.Add("@empl_id", MySqlDbType.Int32).Value = id_employee;
+            cmd1.Parameters.Add("@date_from", MySqlDbType.DateTime).Value = date_from;
+            cmd1.Parameters.Add("@date_to", MySqlDbType.DateTime).Value = date_to;
+            cmd1.Parameters.Add("@organization", MySqlDbType.VarChar).Value = organization;
+            cmd1.Parameters.Add("@position", MySqlDbType.VarChar).Value = position;
+            cmd1.Parameters.Add("@education", MySqlDbType.Int32).Value = education;
+
+            MySqlDataReader reader1 = cmd1.ExecuteReader();
+            if (reader1.HasRows)
+            {
+                while (reader1.Read())
+                {
+                }
+                reader1.Close();
+            }
+            else
+            {
+                reader1.Close();
+                sql = "INSERT INTO `external_practices` (`empl_id`, `date_from`, `date_to`, `organization`, `position`, `education`)"
+                                         + " VALUES (@empl_id, @date_from, @date_to, @organization, @position, @education) ";
+                MySqlCommand cmd2 = new MySqlCommand(sql, Form1.connection);
+
+                cmd2.Parameters.Add("@empl_id", MySqlDbType.Int32).Value = id_employee;
+                cmd2.Parameters.Add("@date_from", MySqlDbType.DateTime).Value = date_from;
+                cmd2.Parameters.Add("@date_to", MySqlDbType.DateTime).Value = date_to;
+                cmd2.Parameters.Add("@organization", MySqlDbType.VarChar).Value = organization;
+                cmd2.Parameters.Add("@position", MySqlDbType.VarChar).Value = position;
+                cmd2.Parameters.Add("@education", MySqlDbType.Int32).Value = education;
+
+                cmd2.ExecuteNonQuery();
+
+            }
+        }
     }
 }
